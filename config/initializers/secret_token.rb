020213818b9be369c27c57b42468b1948b4ba936
Linda Cao <lindacao2000@gmail.com>
2012-04-26 15:26:38 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arisey::Application.config.secret_token = '3c05f001f9e267d8fe4664c0e47d4c5aac710dd01aceca2c3f3798a7e697efa167158defec89a391a7ef1af8c704e6836003de313c1e92356cc30e3ac828e626'
